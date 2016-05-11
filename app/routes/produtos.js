4bd4c8c3c3eca3fa084ef7f2afbdcaad2882de08
brunoguazina <brunoguazina@hotmail.com>
2016-05-11 16:48:49 -0300

    salvando e listando itens

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,17 +1,34 @@
-
 // CONFIGURACAO DAS ROTAS
 module.exports = function (app) {
-	app.get('/produtos',function(req,res){
+
+	var listaProdutos = function(req,res){
 
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = app.infra.produtosBanco;
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
 		
-		produtosBanco.lista(connection,function(erros,resultados){
+		produtosDAO.lista(function(erros,resultados){
 			res.render('produtos/lista',{lista:resultados});
 		}); 
 
 		connection.end();
 
-	});	
+	};
 
+	app.get('/produtos',listaProdutos);	
+
+	app.get('/produtos/form',function(req,res){
+		res.render('produtos/form');
+	});
+
+	app.post('/produtos',function(req,res){
+		
+		var produto = req.body;
+
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.salva(produto,function(erros,resultados){
+			res.redirect('/produtos');
+		});
+
+	});
 };
--- config/express.js
+++ config/express.js
@@ -1,14 +1,17 @@
 // CONFIGURACAO DO EXPRESS
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
-
+// RETORNA PARA O APP
 module.exports = function() {
 
 	var app = express();
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
- 
+ 		
+ 	app.use(bodyParser.urlencoded({extended:true}));
+
 	load('routes',{cwd: 'app'})
 		.then('infra') 
 		.into(app);
