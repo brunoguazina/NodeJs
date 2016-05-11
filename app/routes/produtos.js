57973607a560c53aedf87e0ee84f32c0314fea3d
brunoguazina <brunoguazina@hotmail.com>
2016-05-11 01:06:39 -0300

    criando listagem de produto do banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,20 @@
 // CONFIGURACAO DAS ROTAS
 module.exports = function (app) {
 	app.get('/produtos',function(req,res){
-		res.render('produtos/lista');
+		// INFRA
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host : 'xmysql.equilibrig.com.br',
+			user : 'equilibrig',
+			password : '1234',
+			database : 'equilibrig'
+		});
+		
+		connection.query('select * from livros',function(err,results){
+			res.render('produtos/lista',{lista:results});
+		}); 
+
+		connection.end();
+
 	});	
 }
